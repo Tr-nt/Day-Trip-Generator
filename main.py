0804f7f427d95f4208a92b055a3df1d29e019139
Tr-nt <tdshanepowell@gmail.com>
2022-09-05 19:13:17 -0400

    Created random number generator function

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import time
+import sys
+
+
+#generates random number
+def num_generator():
+    random_num = random.randint(0,6)
+    return random_num
+
+
+#contains destination list and will randomly generate destination for user
+def get_destination():
+    potential_des = ['Tokyo','Norway','Greece','Toronto','Hawaii','Seoul']
+
+def get_restaurant(des_key):
+    destination_keys = {'tokyo': '1', 'norway': ' 2', 'greece': '3','toronto': ' 4', 'hawaii': '5', 'seoul': '6'}
+    
+
+
+

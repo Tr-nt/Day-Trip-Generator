0de98958298b2b3551abf82f6fb9e03eb418c807
Tr-nt <tdshanepowell@gmail.com>
2022-09-05 19:13:17 -0400

    Modified restaurant dictinary in fuction

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ def num_generator(value=6):
 
 #contains destination list and will randomly generate destination for user
 def get_destination(des_key):
-    potential_des = {1: 'tokyo',2: "norway",3: 'greece',4: 'toronto',5: 'hawaii',6: 'seoul'}
+      potential_des = {1: 'Tokyo, Japan',2: "Oslo, Norway",3: 'Athens, Greece',4: 'Toronto, Canada',5: 'Honolulu, Hawaii',6: 'Seoul, South Korea'}
     value = potential_des.get(des_key)
     return value
     
+# contains restaurant dict and will randomly generate restaurant for user
 def get_restaurant(des_name = 'tokyo'):
-    restaurant_choices = {'tokyo': ['a'], 'norway': ['b'],'greece': ['c'], 'toronto': ['d'], 'hawaii': ['e'], 'seoul': ['f']}
+    restaurant_choices = {'Tokyo, Japan': ['Toufuya Ukai','Narisawa','Gyukatsu Motomura Harajuku'], 'Oslo, Norway': ['Restaurant Fjord','Hos Thea','Lofoten Fiskerestaurant'],'Athens, Greece': ['Maiandros Resturant', 'Lithos Tavern', 'Tudor Hall'], 'Toronto, Canada': ['Auberge du Pommier', 'Canoe', 'Richmond Station'], 'Honolulu, Hawaii': ['Senia', 'Moku Kitchen','Ono Seafood'], 'Seoul, South Korea': ['Jungsik Seoul', 'Maple Tree House', 'Ryunique']}
     value = restaurant_choices.get(des_name)
 
     return value 
    
 
 
+
+#num_value = num_generator()
+
+#key_value = get_destination(num_value)
+
+#res_value = get_restaurant(key_value)
+
+#print(num_value,key_value,res_value)
 
 #num = 0 
 """while num != 8:

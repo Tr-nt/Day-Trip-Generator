d9bee0b220941cb4d1144cf884c9ad83743faef6
Tr-nt <89494484+Tr-nt@users.noreply.github.com>
2022-09-06 16:12:55 -0400

    Tested functions for bugs

--- main.py
+++ main.py
@@ -4,34 +4,37 @@ import sys
 
 
 #generates random number
-def num_generator(value=6):
+def num_generator(value=5):
     gen_key = random.randint(0,value)
     return gen_key
 
 
 #contains destination list and will randomly generate destination for user
 def get_destination(des_key):
-      potential_des = {1: 'Tokyo, Japan',2: "Oslo, Norway",3: 'Athens, Greece',4: 'Toronto, Canada',5: 'Honolulu, Hawaii',6: 'Seoul, South Korea'}
+    potential_des = {0: 'Tokyo, Japan',1: "Oslo, Norway",2: 'Athens, Greece',3: 'Toronto, Canada',4: 'Honolulu, Hawaii',5: 'Seoul, South Korea'}
     value = potential_des.get(des_key)
     return value
     
 # contains restaurant dict and will randomly generate restaurant for user
-def get_restaurant(des_name = 'tokyo'):
+def get_restaurant(des_name):
     restaurant_choices = {'Tokyo, Japan': ['Toufuya Ukai','Narisawa','Gyukatsu Motomura Harajuku'], 'Oslo, Norway': ['Restaurant Fjord','Hos Thea','Lofoten Fiskerestaurant'],'Athens, Greece': ['Maiandros Resturant', 'Lithos Tavern', 'Tudor Hall'], 'Toronto, Canada': ['Auberge du Pommier', 'Canoe', 'Richmond Station'], 'Honolulu, Hawaii': ['Senia', 'Moku Kitchen','Ono Seafood'], 'Seoul, South Korea': ['Jungsik Seoul', 'Maple Tree House', 'Ryunique']}
-    value = restaurant_choices.get(des_name)
+    values = restaurant_choices.get(des_name)
 
-    return value 
+    index = num_generator(3)
+
+    list_value = values[index]
+    return list_value 
    
 
 
 
-#num_value = num_generator()
+num_value = num_generator()
 
-#key_value = get_destination(num_value)
+key_value = get_destination(num_value)
 
-#res_value = get_restaurant(key_value)
+res_value = get_restaurant(key_value)
 
-#print(num_value,key_value,res_value)
+print(num_value,key_value,res_value)
 
 #num = 0 
 """while num != 8:

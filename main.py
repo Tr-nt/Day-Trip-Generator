a904b7cecdff3e01205e8a71564d34c8a91c984f
Tr-nt <tdshanepowell@gmail.com>
2022-09-05 20:26:03 -0400

    modified number generator function

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ import sys
 
 
 #generates random number
-def num_generator():
-    random_num = random.randint(0,6)
-    return random_num
+def num_generator(value=6):
+    gen_key = random.randint(0,value)
+    return gen_key
 
 
 #contains destination list and will randomly generate destination for user
 def get_destination(des_key):
-    potential_des = ['Tokyo','Norway','Greece','Toronto','Hawaii','Seoul']
+    potential_des = {1: 'tokyo',2: "norway",3: 'greece',4: 'toronto',5: 'hawaii',6: 'seoul'}
+    value = potential_des.get(des_key)
+    return value
+    
+def get_restaurant(des_name = 'tokyo'):
+    restaurant_choices = {'tokyo': ['a'], 'norway': ['b'],'greece': ['c'], 'toronto': ['d'], 'hawaii': ['e'], 'seoul': ['f']}
+    value = restaurant_choices.get(des_name)
 
-def get_restaurant(des_key):
-    destination_keys = {1 : 'tokyo', 2 : 'norway', 3 : 'greece',4 : 'toronto', 5: 'hawaii', 6: 'seoul'}
-    value = destination_keys.get(des_key)
     return value 
    
 
+
+
+#num = 0 
+"""while num != 8:
+    num = num_generator(8)
+    print(num)"""
+#test = get_restaurant()
+#print(test)
 
 
